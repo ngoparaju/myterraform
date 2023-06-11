d7808b24b37c3977b13f3f493d4ef2cc1572a22b
ngoparaju <n.goparaju@gmail.com>
2023-06-11 22:45:12 +0100

    test

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAVH6QNN44JEGO2QMO"
-  secret_key = "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"
+ 
 }
 resource "aws_instance" "myec2" {
   ami           = "ami-0889a44b331db0194"
