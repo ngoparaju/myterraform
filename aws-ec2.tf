8d9ececc521cd00d9e8999b2c448e68820e6ecb2
ngoparaju <n.goparaju@gmail.com>
2023-07-24 20:18:38 +0100

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -1,12 +1,20 @@
-provider "aws" {
-  region     = "us-east-1"
- 
+terraform {
+ required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.16"
 }
-resource "aws_instance" "myec2" {
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "myfirst"
-  }
+}
+        required_version = ">= 1.2.0"
+}
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "aws_ec2_demo" {
+   count = 2
+   ami = "ami-05548f9cecf47b442" 
+   instance_type = "t2.micro"
+   tags = {
+     Name = "terraform_demo"
+}
 }
